bc32e5ac6bea1d67e797a07a328621980a371edc
kevinhuang <kevinhpf@outlook.com>
2019-08-22 21:57:15 +0800

    test push

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,7 +1,7 @@
 package com.test;
 
-public class test {
-    public  static  void main(String[] args){
+public class Test{
+    public static void main(String[] args){
         System.out.println("Hello IDEA");
     }
 }
