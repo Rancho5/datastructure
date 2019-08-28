ef8b9057c4d4bb9354fe351ddc68e7ec9bd7a8af
kevinhuang <kevinhpf@outlook.com>
2019-08-28 22:01:58 +0800

    Queue version1

--- src/com/test/Queue.java
+++ src/com/test/Queue.java
@@ -1,4 +1,35 @@
 package com.test;
 
-public class Queue {
+//链式队列	在LinkedList基础上实现
+public class Queue<T> {
+    LinkedList<T> qList = new LinkedList<>();
+
+    public void add(T obj){
+        qList.add(obj);
+    }
+
+    public void poll(){
+        qList.remove(0);
+    }
+
+    public int contains(Object obj){
+        return qList.contains(obj);
+    }
+
+    public void printData(){
+        qList.printData();
+    }
+
+    public static void main(String[] args){
+        Queue<Integer> q = new Queue<>();
+        for(int i = 0; i < 5; i++)
+            q.add(i);
+        q.printData();
+        q.poll();
+        q.printData();
+        System.out.println(q.contains(3));
+        q.add(null);
+        q.printData();
+    }
+
 }
