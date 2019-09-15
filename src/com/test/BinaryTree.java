745ffbf32941125318a0bcbb55611d03484390f2
Rancho5 <751259268@qq.com>
2019-09-15 11:50:42 +0800

    BinaryTree

--- src/com/test/BinaryTree.java
+++ src/com/test/BinaryTree.java
@@ -24,10 +24,82 @@ public class BinaryTree {
      *     B       C
      * D      E        F
      */
+    public void createBinaryTree(){
+        TreeNode<String> nodeB = new TreeNode<>(2, "B");
+        TreeNode<String> nodeC = new TreeNode<>(3, "C");
+        TreeNode<String> nodeD = new TreeNode<>(4, "D");
+        TreeNode<String> nodeE = new TreeNode<>(5, "E");
+        TreeNode<String> nodeF = new TreeNode<>(6, "F");
+        root.leftChild = nodeB;
+        root.rightChild = nodeC;
+        nodeB.leftChild = nodeD;
+        nodeB.rightChild = nodeE;
+        nodeC.rightChild = nodeF;
+    }
+
+    /**
+     * 求二叉树高度
+     *
+     */
+    public int getHeight(){
+        return getHeight(root);
+    }
+
+    private int getHeight(TreeNode node){
+        if(node == null)
+            return 0;
+        int i = getHeight(node.leftChild);
+        int j = getHeight(node.rightChild);
+        return (i>j)?i+1:j+1;
+    }
+
+    /**
+     * 获取二叉树结点数
+     *
+     */
+    public int getSize(){
+        return getSize(root);
+    }
+
+    private int getSize(TreeNode node){
+        if(node == null)
+            return 0;
+        return getSize(node.rightChild) + getSize(node.leftChild)+1;
+    }
+
+    /**
+     * 前序遍历（迭代法）
+     *
+     */
+    public void preOrder(TreeNode node){
+        if(node == null)
+            return;
+        System.out.println(node.data);
+        preOrder(node.leftChild);
+        preOrder(node.rightChild);
+    }
+
+    /**
+     * 前序遍历（非迭代）
+     *
+     */
+    public void nonRecOrder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(node);
+        while(!s.isEmpty()){
+            TreeNode n = s.pop();
+            if(n.rightChild != null)
+                s.push(n.rightChild);
+            if(n.leftChild != null)
+                s.push(n.leftChild);
+            System.out.println(n.data);
+        }
 
 
-
-
+    }
 
 
     public class TreeNode<T>{
@@ -59,6 +131,17 @@ public class BinaryTree {
             this.data = data;
         }
 
+    }
+
+    public static void main(String[] args){
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.createBinaryTree();
+        int height = binaryTree.getHeight();
+        System.out.println("treeHeihgt:"+height);
+        int size = binaryTree.getSize();
+        System.out.println("treeSize:"+size);
+		binaryTree.preOrder(binaryTree.root);
+//      binaryTree.nonRecOrder(binaryTree.root);
     }
 
 }
