b69572fbd95a6c9c9030981f45eaf49c6700dad9
Rancho5 <751259268@qq.com>
2019-11-06 17:53:07 +0800

    递归-4：求阶乘

--- src/com/recursion/Factorial.java
+++ src/com/recursion/Factorial.java
@@ -2,5 +2,13 @@ package com.recursion;
 
 //求阶乘
 public class Factorial {
+    public static int fac(int n){
+        if(n == 0)
+            return 1;
+        return n*fac(n-1);
+    }
 
+    public static void main(String[] args) {
+        System.out.println(fac(15));
+    }
 }
