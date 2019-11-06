c39276e90f3663d7bf692958c3f7fb1c7c249733
Rancho5 <751259268@qq.com>
2019-11-06 17:50:41 +0800

    递归-3：欧几里得算法求最大公约数

--- src/com/recursion/Gcd.java
+++ src/com/recursion/Gcd.java
@@ -1,5 +1,16 @@
 package com.recursion;
 
+//欧几里得算法求最大公约数
 public class Gcd {
+    public static int gcd(int n, int m){
+        if(m == 0){
+            return n;
+        }else{
+            return gcd(m, n%m);
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println(gcd(33,55));
+    }
 }
